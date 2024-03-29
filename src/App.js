f0ace2dcea4d8e36844347d1d9296834cc8c750d
Sunny Leonarde <163378147+Leo92051@users.noreply.github.com>
2024-03-30 00:33:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const incrementCounter = () => {
+    setCounter(prevCounter => prevCounter + 1);
+  };
+
+  const decrementCounter = () => {
+    setCounter(prevCounter => prevCounter - 1);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload!!!!!------GITHUB-ACTIONS.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Rajinikanth VAdla DevOps Application</h1>
+        <p>Counter: {counter}</p>
+        <div>
+          <button onClick={incrementCounter}>Increment</button>
+          <button onClick={decrementCounter}>Decrement</button>
+        </div>
       </header>
     </div>
   );
